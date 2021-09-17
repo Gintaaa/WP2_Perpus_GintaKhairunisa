91e4f8ae64ab41fcffcf2a906d65472c1eddc928
Gintaaa <gintakhairunisa@gmail.com>
2021-09-17 10:08:40 +0800

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama Saya Ginta Khairunisa
+              Saya tinggal di daerah Jakarta
+              Olah Raga yang saya sukai adalah Ranang dan Basket";
+    }
+}
