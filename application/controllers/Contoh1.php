82c654b2788cc34c0d977217c8787916ad3b7395
Gintaaa <gintakhairunisa@gmail.com>
2021-09-27 21:34:03 +0800

    Web programming pertemuan 3 pembelajaran MVC

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan<h1>";
-        echo "Nama Saya Ginta Khairunisa
-              Saya tinggal di daerah Jakarta
-              Olah Raga yang saya sukai adalah Ranang dan Basket";
+        echo "<h3>Nama Saya Ginta Khairunisa</h3>
+              <p>Saya tinggal di daerah Jakarta</p>
+              <p>Olah Raga yang saya sukai adalah Ranang dan Basket</p>";
     }
-}
+} 
---
+++ application/views/View_latihan.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+    <link href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" rel="stylesheet"> 
+    <style type="text/css">
+    	h1{
+    		font-family: 'Ephesis', cursive;
+    		font-color: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);
+			}
+    </style>
+</head>
+
+<body>
+    <h1>Halo Kawan..</h1>
+    <h3>Yuk kita belajar web programming..!!!</h3>
+    Nilai 1 = <?= $nilai1; ?><br>
+    Nilai 2 = <?= $nilai2; ?><br>
+    ini hasil dari permodelan dengan methode penjumlahan yaitu 
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>  
+</body>
+
+</html>
